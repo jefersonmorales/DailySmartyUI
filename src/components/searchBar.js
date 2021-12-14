07fe0f3b462d59ec4bd4d9d996e7038f0b6a7f7e
jfermorales <jfermorales28@outlook.com>
2021-12-13 21:09:58 -0600

    Created recent posts and SearchBar components and Redux-Form installed

---
+++ src/components/searchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form>
+        <input placeholder="Daily Smarty Search Bar" />
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
