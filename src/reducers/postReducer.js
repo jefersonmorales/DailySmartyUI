103101651701348e94e4cd0f49fdccae054ebfcb
jefersonmorales <jfermorales28@outlool.com>
2021-12-15 19:38:37 -0600

    Fixed the error in postReducerComponent and Updated README.md

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,17 +1,15 @@
-import {
-  SET_RECENT_POSTS
-} from '../actions/types';
+import { SET_RECENT_POSTS } from "../actions/types";
 
-const INIT_STATE {
+const INIT_STATE = {
   posts: [],
-  recentPosts: []
-}
+  recentPosts: [],
+};
 
-export default function(state = INIT_STATE, action) {
+export default function (state = INIT_STATE, action) {
   switch (action.type) {
     case SET_RECENT_POSTS:
-      
-    return [...state, recentPosts: action.payload]
+      const recentPosts = action.payload;
+      return [...state, recentPosts];
     default:
       return state;
   }
